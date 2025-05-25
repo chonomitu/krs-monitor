bd9150d90b0fa5a4b8929c23b6083b2248bdb29e
kyry <chonomitu@gmail.com>
2025-05-25 18:47:13 +0200

    Całkowite wyczyszczenie repo

--- Documents/WEB KRS/krs_monitor/config.py
+++
@@ -1,9 +0,0 @@
-
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-RPC_URL = os.getenv("ARBITRUM_RPC_URL")
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
